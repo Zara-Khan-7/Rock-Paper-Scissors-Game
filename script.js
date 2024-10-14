95204e082cd24b8450a2ced1be8da4e8f7897729
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-10-15 04:38:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const choiceBtns = document.querySelectorAll('.choice-btn')
+
+const playerChoiceText = document.querySelector('.player-choice-text')
+const cpuChoiceText = document.querySelector('.cpu-choice-text')
+
+const gameTitle = document.querySelector(".game-title")
+
+const scoreWonText = document.querySelector(".score-won-text")
+const scoreDrawText = document.querySelector(".score-draw-text")
+const scoreLostText = document.querySelector(".score-lost-text")
+
